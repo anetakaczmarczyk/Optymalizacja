12363aff122ba91c2ed600ab1e39b8f71cb3500a
aKaczmarczyk1 <akaczmarczyk@student.agh.edu.pl>
2024-11-13 23:59:07 +0100

    Implement file-saving for all assignments

--- Optymalizacja/Pliki/opt_alg.cpp
+++ Optymalizacja/Pliki/opt_alg.cpp
@@ -240,7 +240,9 @@ solution HJ(matrix (*ff)(matrix, matrix, matrix), matrix x0, double s, double al
         solution XB, X;
         XB.x = x0;
         XB.fit_fun(ff, ud1, ud2);
+        // std::stringstream HJ_ss; // do zapisu danych
         while (s > epsilon) {
+             // HJ_ss << m2d(XB.x(0)) << ";" <<  m2d(XB.x(1))<< ";\n";
             X = HJ_trial(ff, XB, s, ud1, ud2);
             if (X.y < XB.y) {
                 do {
@@ -262,6 +264,14 @@ solution HJ(matrix (*ff)(matrix, matrix, matrix), matrix x0, double s, double al
                 break;
             }
         }
+        // zapis wynikow do pliku
+        // std::ofstream file(R"(C:\Users\Animatt\CLionProjects\Optymalizacja\Optymalizacja\lab2-analiza\lab2-HJ-przyklad.txt)"); //musialam dac cala sciezke bo nie dzialalo xd
+        // if (file.is_open()) {
+        // 	file << HJ_ss.str();
+        // 	file.close();
+        // }else {
+        // 	cerr << "Nie udało się otworzyć pliku do zapisu.\n";
+        // }
         return X;
     } catch (string ex_info) {
         throw ("solution HJ(...):\n" + ex_info);
@@ -271,7 +281,7 @@ solution HJ(matrix (*ff)(matrix, matrix, matrix), matrix x0, double s, double al
 
 solution HJ_trial(matrix (*ff)(matrix, matrix, matrix), solution XB, double s, matrix ud1, matrix ud2) {
     try {
-        int n = get_len(XB.x); // Bo 2 wymiary, byłoby 3 jakby 3 wymiary
+        int n = get_len(XB.x);
         matrix e = matrix(n, n);
         for (int i = 0; i < n; i++) {
             e(i, i) = 1.0;
@@ -289,7 +299,9 @@ solution HJ_trial(matrix (*ff)(matrix, matrix, matrix), solution XB, double s, m
             } else if (f2_sol.y < XB.y) {
                 XB = f2_sol;
             }
+
         }
+
         return XB;
     } catch (string ex_info) {
         throw ("solution HJ_trial(...):\n" + ex_info);
@@ -315,7 +327,7 @@ solution Rosen(matrix (*ff)(matrix, matrix, matrix), matrix x0, matrix s0, doubl
         solution Xopt;
         //Tu wpisz kod funkcji
         int n = get_len(x0);
-
+        // std::stringstream Rosen_ss; // do zapisu danych
         matrix e = matrix(n, n);
         for (int i = 0; i < n; i++) {
             e(i, i) = 1.0;
@@ -330,6 +342,7 @@ solution Rosen(matrix (*ff)(matrix, matrix, matrix), matrix x0, matrix s0, doubl
         XB.x = x0;
         XB.fit_fun(ff, ud1, ud2);
         while (max(s) > epsilon) {
+            // Rosen_ss << m2d(XB.x(0)) << ";" <<  m2d(XB.x(1))<< ";\n";
             for (int j = 0; j < n; j++) {
                 solution temp;
                 temp.x = XB.x + s(j) * e[j];
@@ -384,7 +397,15 @@ solution Rosen(matrix (*ff)(matrix, matrix, matrix), matrix x0, matrix s0, doubl
         //zlozona z lambd
         //zaimplementowac w 2D
 
-
+        // zapis wynikow do pliku
+    //     std::ofstream file(R"(C:\Users\Animatt\CLionProjects\Optymalizacja\Optymalizacja\lab2-analiza\lab2-Rosen-przyklad.txt)"); //musialam dac cala sciezke bo nie dzialalo xd
+    //     if (file.is_open()) {
+    //         file << Rosen_ss.str();
+    //         file.close();
+    //     }else {
+    //         cerr << "Nie udało się otworzyć pliku do zapisu.\n";
+    //     }
+    //
         return Xopt;
     } catch (string ex_info) {
         throw ("solution Rosen(...):\n" + ex_info);
